b649b5bfcf787b0a400eae5d715ae71eb286909d
tuubacann <canntuba08@gmail.com>
2026-04-24 21:34:27 +0300

    Add user system endpoints

---
+++ backend/app/database.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+
+MONGO_URL = "mongodb://mongodb:27017"
+DATABASE_NAME = "word_crush_db"
+
+client = MongoClient(MONGO_URL)
+db = client[DATABASE_NAME]
+
+users_collection = db["users"]
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from app.database import users_collection
 
 app = FastAPI(title="Word Crush API")
 
 @app.get("/")
 def home():
     return {"message": "Word Crush backend is running"}
+
+@app.post("/api/users")
+def create_user(username: str = Body(...)):
+    user = {
+        "username": username,
+        "gold": 1000
+    }
+
+    result = users_collection.insert_one(user)
+
+    return {
+        "message": "User created",
+        "user_id": str(result.inserted_id)
+    }
+@app.get("/api/users")
+def get_users():
+    users = list(users_collection.find({}, {"_id": 0}))
+    return users
